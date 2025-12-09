ea9021a1359abbd3a031481c0a036193ccee52a1
Hagan Farrar <haganfarrar@Hagans-MacBook-Air.local>
2025-12-08 19:46:24 -0600

    Switch hero image to Outdoor Kitchen photo

--- app/page.tsx
+++ app/page.tsx
@@ -59,7 +59,7 @@ export default function HomePage() {
       <section className="relative">
         <div className="relative h-[70vh] md:h-[80vh]">
           <img
-            src="/images/services/hardscaping/hardscaping-services-in-colleyville-tx-scaled.jpg"
+            src="/images/services/hardscaping/Backyard-Hardscape-Idea-With-Outdoor-Kitchen.webp"
             alt="Colleyville Landscaping"
             className="w-full h-full object-cover"
           />
